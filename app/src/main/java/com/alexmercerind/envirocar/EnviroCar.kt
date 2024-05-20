04f3192c584e2fbfe50e5b82eeea2948edc46e99
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2024-05-20 23:18:50 +0530

    feat: Google Maps support

--- app/src/main/java/com/alexmercerind/envirocar/EnviroCar.kt
+++ app/src/main/java/com/alexmercerind/envirocar/EnviroCar.kt
@@ -1,13 +1,7 @@
 package com.alexmercerind.envirocar
 
 import android.app.Application
-import com.mapbox.mapboxsdk.Mapbox
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class EnviroCar : Application() {
-    override fun onCreate() {
-        super.onCreate()
-        Mapbox.getInstance(this)
-    }
-}
+class EnviroCar : Application()
--- app/src/main/java/com/alexmercerind/envirocar/ui/MainActivity.kt
+++ app/src/main/java/com/alexmercerind/envirocar/ui/MainActivity.kt
@@ -1,25 +1,23 @@
 package com.alexmercerind.envirocar.ui
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.content.res.AppCompatResources
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.alexmercerind.envirocar.R
 import com.alexmercerind.envirocar.databinding.ActivityMainBinding
-import com.alexmercerind.envirocar.util.Constants
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolylineOptions
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.gson.GsonBuilder
-import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
-import com.mapbox.mapboxsdk.geometry.LatLng
-import com.mapbox.mapboxsdk.geometry.LatLngBounds
-import com.mapbox.mapboxsdk.plugins.annotation.LineManager
-import com.mapbox.mapboxsdk.plugins.annotation.LineOptions
-import com.mapbox.mapboxsdk.plugins.annotation.SymbolManager
-import com.mapbox.mapboxsdk.plugins.annotation.SymbolOptions
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.filterNotNull
@@ -40,7 +38,13 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        lifecycleScope.launch(Dispatchers.IO) {
+        val map = SupportMapFragment.newInstance()
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.mapView, map)
+            .commit()
+
+        lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
                 viewModel.details.filterNotNull().collect { result ->
                     withContext(Dispatchers.Main) {
@@ -63,66 +67,57 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
-        lifecycleScope.launch(Dispatchers.IO) {
+        lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
                 viewModel.points.filterNotNull().collect { result ->
                     withContext(Dispatchers.Main) {
 
-                        binding.mapView.getMapAsync { map ->
-                            map.setStyle(Constants.MAP_STYLE)
-                            map.setMinZoomPreference(1.0)
-                            map.setMaxZoomPreference(20.0)
+                        map.getMapAsync { map ->
+                            map.setMinZoomPreference(1.0F)
+                            map.setMaxZoomPreference(20.0F)
+
+                            val latLngBoundsBuilder = LatLngBounds.builder()
 
                             val points = result.map {
-                                LatLng(it.geometry.coordinates[1], it.geometry.coordinates[0])
+                                LatLng(
+                                    it.geometry.coordinates[1],
+                                    it.geometry.coordinates[0]
+                                ).also { latLng ->
+                                    latLngBoundsBuilder.include(latLng)
+                                }
                             }
 
-                            map.getStyle { style ->
+                            map.addPolyline(
+                                PolylineOptions()
+                                    .addAll(points)
+                                    .width(4.0F)
+                                    .color(Color.parseColor("#000000"))
+                            )
 
-                                style.addImage(
-                                    MARKER,
-                                    AppCompatResources.getDrawable(
-                                        this@MainActivity,
-                                        R.mipmap.ic_symbol_icon
-                                    )!!
+                            // Add markers to start & end position.
+
+                            // Start position.
+                            map.addMarker(
+                                MarkerOptions()
+                                    .position(points.first())
+                                    .contentDescription(getString(R.string.start))
+                            )
+
+                            // End position.
+                            map.addMarker(
+                                MarkerOptions()
+                                    .position(points.last())
+                                    .contentDescription(getString(R.string.end))
+                            )
+
+                            // Move camera to show available points.
+
+                            map.moveCamera(
+                                CameraUpdateFactory.newLatLngBounds(
+                                    latLngBoundsBuilder.build(),
+                                    80
                                 )
-
-                                val lineManager = LineManager(binding.mapView, map, style)
-                                val symbolManager = SymbolManager(binding.mapView, map, style)
-
-                                lineManager.create(
-                                    LineOptions()
-                                        .withLatLngs(points)
-                                        .withLineWidth(2.0F)
-                                        .withLineColor("#000000")
-                                )
-
-                                // Add markers to start & end position.
-
-                                // Start position.
-                                symbolManager.create(
-                                    SymbolOptions()
-                                        .withIconImage(MARKER)
-                                        .withIconSize(1.5F)
-                                        .withLatLng(points.first())
-                                        .withTextAnchor(getString(R.string.start))
-                                )
-
-                                // End position.
-                                symbolManager.create(
-                                    SymbolOptions()
-                                        .withIconImage(MARKER)
-                                        .withIconSize(1.5F)
-                                        .withLatLng(points.last())
-                                        .withTextAnchor(getString(R.string.end))
-                                )
-
-                                // Move camera to show available points.
-                                val bounds = LatLngBounds.fromLatLngs(points)
-                                map.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 80))
-                            }
-
-
+                            )
                         }
                     }
                 }
